3fc296ede926d87a1fc3cfdeacf52115cac5a779
AustinNolfi <58268419+AustinNolfi@users.noreply.github.com>
2020-03-04 13:25:05 -0500

    Create TravProf.java

---
+++ TravProf.java
@@ -0,0 +1,12 @@
+public class TravProf {
+    String travAgentID;
+    String firstName;
+    String lastName;
+    String address;
+    String phone;
+    float tripCost;
+    String travelType;
+    String paymentType;
+    MedCond medCondInfo;
+    public TravProf();
+}
