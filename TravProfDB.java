4ce6fdfeddae0e12e926150dd614441aeaaafa06
AustinNolfi <58268419+AustinNolfi@users.noreply.github.com>
2020-03-22 20:24:26 -0400

    Update TravProfDB.java

--- TravProfDB.java
+++ TravProfDB.java
@@ -8,23 +8,29 @@ public class TravProfDB {
     String fileName;
 
     public TravProfDB(String fileAddress) {
+        fileName = fileAddress;
     }
 
-public void insertNewProfile(TravProf profile){
-    travelerList.add(profile);
-}
+    public void insertNewProfile(TravProf profile){
+        //add profile to ArrayList
+        travelerList.add(profile);
+    }
 
-public TravProf findProfile(String ID, String lastName){
-}
+    public TravProf findProfile(String ID, String lastName){
+        //return profile from DB by ID and lastName
+        int conversion = Integer(ID);
+        TravProf profile = travelerList.get(conversion);
+        return profile;
+    }
 
-public void writeAllTravProf(){
-    //write all TravProf in travelerList to a file
-}
+    public void writeAllTravProf(){
+        //write all TravProf in travelerList to a file
+        
+    }
 
-public void initializeDatabase(){
-
-}
+    public void initializeDatabase(){
+        //read all Trav prof from file
+    }
     public static void main(String[] args){
-
     }
 }
